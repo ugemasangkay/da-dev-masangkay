36b56e120a2ca4284bbb8ea3c658f11c66df6055
unknown <ugemasangkay@gmail.com>
2023-06-19 19:36:04 +0800

    feat: add migrations, controller, and model file for Todo

---
+++ app/Http/Controllers/TodoController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Todo;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+
+class TodoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $todos = Todo::orderBy('created_at', 'desc')->get();
+        return response()->json($todos);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required|max:255',
+        ]);
+
+        $todo = Todo::create([
+            'title' => $request->title,
+        ]);
+        Log::info('TODO_CREATE', $todo->toArray());
+
+        return response()->json($todo, 201);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $todo = Todo::findOrFail($id);
+        Log::info('TODO_UPDATE', $todo->toArray());
+        $todo->update([
+            'completed' => !$todo->completed,
+        ]);
+        Log::info('TODO_UPDATED', $todo->toArray());
+
+        return response()->json($todo);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $todo = Todo::findOrFail($id);
+        Log::info('TODO_DELETE', $todo->toArray());
+        $todo->delete();
+
+        return response()->json(null, 204);
+    }
+}
